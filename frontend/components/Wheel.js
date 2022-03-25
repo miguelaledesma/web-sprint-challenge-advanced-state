cea0444bdc4db1e38483744577cea5ab5c871e31
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-24 17:32:30 -0700

    wooohoo got some tests finally passing

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -21,16 +21,24 @@ export function Wheel(props) {
     <div id="wrapper">
       <div id="wheel">
       <div className={`${props.wheel === 0 ? "cog active" : "cog"}`} style={{ "--i": 0 }}>
-      {props.wheel === 0 ? "B" : ""} </div>
+    {props.wheel === 0 ? "B": ""}</div>
         
       <div className={`${props.wheel === 1 ? "cog active" : "cog"}`} style={{ "--i": 1 }}>
-      {props.wheel === 1 ? "B" : ""} </div>
+    {props.wheel === 1 ? "B": ""}</div>
         
       <div className={`${props.wheel === 2 ? "cog active" : "cog"}`} style={{ "--i": 2 }}>
-      {props.wheel === 2 ? "B" : ""} </div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+    {props.wheel === 2 ? "B": ""}</div>
+        
+      <div className={`${props.wheel === 3 ? "cog active" : "cog"}`} style={{ "--i": 3 }}>
+    {props.wheel === 3 ? "B": ""}</div>
+
+      <div className={`${props.wheel === 4 ? "cog active" : "cog"}`} style={{ "--i": 4 }}>
+    {props.wheel === 4 ? "B": ""}</div>
+
+      <div className={`${props.wheel === 5 ? "cog active" : "cog"}`} style={{ "--i": 5 }}>
+    {props.wheel === 5 ? "B": ""}</div>
+        
+        {/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
         
@@ -43,7 +51,7 @@ export function Wheel(props) {
 
 const mapState = state => {
   return {
-    wheel: state.wheel.counter, 
+    wheel:state.wheel.counter, 
   }
 }
 

b8252bf7b335fafdb69b4b06e1783b7f4c6087d6
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-25 17:17:39 -0700

    fixed message component

--- frontend/components/Message.js
+++ frontend/components/Message.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux'
 import * as type from '../state/action-creators'
 
 export function Message(props) {
-  const { message } = props
-  return <div id="message">{message}</div>
+  const { infoMessage } = props
+  return <div id="message">{infoMessage}</div>
 }
 
 
